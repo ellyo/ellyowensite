0f6107688eb65138a7eaf55ef8f47ce6faf51f9c
Elly Owen <elly.o@hotmail.co.uk>
2018-07-28 11:51:12 +0100

    Changed fonts and colours

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ROOT_PATH = os.path.dirname(__file__)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -25,7 +27,7 @@ SECRET_KEY = 'pvv8i$q=7#1+8byu^-4$q=mq%%kr)kz^^(7kx*g6ix9l)#=!-b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.ellyowen.co.uk']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.ellyowen.co.uk', 'localhost']
 
 
 # Application definition
@@ -120,5 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(ROOT_PATH, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
